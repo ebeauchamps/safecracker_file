f57a10f38e6bb260606a1103693d74769dc9724f
Emmanuel Beauchamps <e.bops@hotmail.fr>
2013-01-25 21:47:01 +0100

    First commit

---
+++ french/safecracker_file_lang.php
@@ -0,0 +1,32 @@
+<?php
+
+$lang = array(
+	'choose_upload_dir' => 'Répertoire',
+	'file_type' => 'Type',
+	'no_upload_dir' => 'Aucun répertoire de téléchargement n\'est défini.',
+	'choose_existing' => 'Ou sinon choisissez&hellip;',
+	'show_existing' => 'Afficher les fichiers existants ?',
+	'num_existing' => 'Nombre de fichiers à afficher'
+);
+
+/* upload */
+$lang['upload_userfile_not_set'] = "Impossible de trouver une variable 'post' nommée 'userfile'.";
+$lang['upload_file_exceeds_limit'] = "La taille du fichier téléchargé dépasse la valeur maximale définie dans votre fichier de configuration PHP.";
+$lang['upload_file_exceeds_form_limit'] = "La taille du fichier téléchargé dépasse la valeur maximale autorisée par le formulaire de soumission.";
+$lang['upload_file_partial'] = "Le fichier n'a été que partiellement téléchargé.";
+$lang['upload_no_temp_directory'] = "Le répertoire temporaire est absent ou inaccessible.";
+$lang['upload_unable_to_write_file'] = "Le fichier n'a pas pu être écrit sur le disque.";
+$lang['upload_stopped_by_extension'] = "Le téléchargement du fichier a été interrompu par une extension.";
+$lang['upload_no_file_selected'] = "Vous n'avez pas sélectionné de fichier à télécharger.";
+$lang['upload_invalid_filetype'] = "Le type de fichier que vous tentez de télécharger n'est pas autorisé.";
+$lang['upload_invalid_filesize'] = "Le fichier que vous tentez de télécharger est d'une taille supérieure à celle autorisée.";
+$lang['upload_invalid_dimensions'] = "L'image que vous tentez de télécharger dépasse les limites fixées pour la hauteur et/ou la largeur.";
+$lang['upload_destination_error'] = "Un problème est survenu en tentant de déplacer le fichier téléchargé vers sa destination finale.";
+$lang['upload_no_filepath'] = "Le chemin de téléchargement ne semble pas valide.";
+$lang['upload_no_file_types'] = "Vous n'avez précisé aucun type de fichier autorisé.";
+$lang['upload_bad_filename'] = "Le nom de fichier que vous avez soumis existe déjà sur le serveur.";
+$lang['upload_not_writable'] = "Le répertoire de téléchargement ne semble pas inscriptible.";
+$lang['upload_destination_does_not_exist'] = 'Le répertoire de téléchargement sélectionné pour le champ Fichier SafeCracker n\'existe plus.';
+
+/* End of file lang.safecracker_file.php */
+/* Location: ./system/expressionengine/third_party/safecracker_file/language/french/lang.safecracker_file.php */
